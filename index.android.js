20fcb1b493a5ca49d5f52f52e915af65f7351128
VahanAper <asryanvahandev@gmail.com>
2017-05-11 15:55:37 +0400

    final

--- index.android.js
+++ index.android.js
@@ -1,53 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
+  View,
   Text,
-  View
+  TouchableHighlight,
+  Dimensions,
 } from 'react-native';
 
-export default class gallery extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const Images = [
+  {
+    uri: 'https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1509a.jpg',
+    label: 'Westerlund 2 — Hubble’s 25th anniversary image',
+  },
+  {
+    uri: 'https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1501a.jpg',
+    label: 'New view of the Pillars of Creation — visible',
+  },
+  {
+    uri: 'https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1107a.jpg',
+    label: 'A rose made of galaxies',
+  },
+  {
+    uri: 'https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0715a.jpg',
+    label: 'Extreme star cluster bursts into life in new Hubble image',
+  },
+  {
+    uri: 'https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1608a.jpg',
+    label: 'The Bubble Nebula',
+  },
+  {
+    uri: 'https://cdn.spacetelescope.org/archives/images/wallpaper2/potw1345a.jpg',
+    label: 'Antennae Galaxies reloaded',
+  },
+];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000000',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  image: {
+    flex: 99,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  instructions: {
+  imageLabel: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    color: 'white',
+    padding: 2,
   },
 });
+
+export default class gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.onImageLayout = this.onImageLayout.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+  }
+
+  state = {
+    index: 0,
+    imageWidth: null,
+    width: Dimensions.get('window').width,
+  }
+
+  onImageLayout(event) {
+    this.setState({
+      imageWidth: event.nativeEvent.layout.width,
+      width: Dimensions.get('window').width,
+    });
+  }
+
+  nextImage(event) {
+    const { index, imageWidth } = this.state;
+    const X = event.nativeEvent.locationX;
+    const delta = (X < imageWidth / 2) ? -1 : 1;
+
+    let newIndex = (index + delta) % Images.length;
+
+    if (newIndex < 0) {
+      newIndex = Images.length - Math.abs(newIndex);
+    }
+
+    this.setState({
+      index: newIndex,
+    });
+  }
+
+  render() {
+    const image = Images[this.state.index];
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight
+          style={[styles.image, { width: this.state.width }]}
+          onPress={this.nextImage}
+        >
+          <Image
+            source={{ uri: image.uri }}
+            style={[styles.image, { width: this.state.width }]}
+            onLayout={this.onImageLayout}
+            resizeMode="contain"
+          >
+            <Text style={[styles.imageLabel, { width: this.state.imageWidth }]}>{image.label}</Text>
+          </Image>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
 
 AppRegistry.registerComponent('gallery', () => gallery);
